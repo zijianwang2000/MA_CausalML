4234941f420502a1a8b270d64a0df05534708371
zijianwang2000 <s6ziwang@uni-bonn.de>
2024-05-07 22:08:18 +0200

    Updates

--- Complex model (10 confounders)/XGBoost/MC_simulation.py
+++ Complex model (10 confounders)/XGBoost/MC_simulation.py
@@ -3,7 +3,7 @@ import numpy as np
 import xgboost as xgb
 import pickle
 from data_generation import m_0, g_0, get_data
-from dml_algorithm import mm_ate, dml_no_cf_ate, dml_ate
+from dml_algorithm import mm_ate, dml_ate
 
 
 # Load tuned hyperparameters of XGBoost
@@ -24,19 +24,17 @@ def get_models(xgb_params_dict):
 
 
 # MC simulation for a given sample size N
-def mc_simulation(N, n_MC=5000):
+def mc_simulation(N, n_MC=2000):
     rng = np.random.default_rng(seed=123)
-    ate_estimates = np.empty((n_MC, 8))
-    sigma_estimates = np.empty((n_MC, 2))
-    CIs = np.empty((n_MC, 4))
+    ate_estimates = np.empty((n_MC, 4))
+    sigma_estimates = np.empty(n_MC)
+    CIs = np.empty((n_MC, 2))
 
     for j in range(n_MC):
         y_data, d_data, x_data = get_data(N, rng)
         ate_estimates[j, 0] = mm_ate(y_data, d_data, x_data, g_0, m_0)
         model_g, model_m = get_models(opt_params_xgboost[N][j])
-        ate_estimates[j, 1] = dml_no_cf_ate(y_data, d_data, x_data, model_g, model_m)
-        ate_estimates[j, 2:5], sigma_estimates[j, 0], CIs[j, :2] = dml_ate(y_data, d_data, x_data, model_g, model_m, K=2)
-        ate_estimates[j, 5:], sigma_estimates[j, 1], CIs[j, 2:] = dml_ate(y_data, d_data, x_data, model_g, model_m, K=5)
+        ate_estimates[j, 1:], sigma_estimates[j], CIs[j] = dml_ate(y_data, d_data, x_data, model_g, model_m)
 
     return [ate_estimates, sigma_estimates, CIs]
 
