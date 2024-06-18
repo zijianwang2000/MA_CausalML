0335bac8fc37e3d9a0de29d0b8ae4863973e6894
zijianwang2000 <s6ziwang@uni-bonn.de>
2024-06-18 14:20:37 +0200

    Cleaning

---
+++ Simple model (3 confounders) - Clip x_normal/XGBoost_hyperparameters.py
@@ -0,0 +1,53 @@
+# Cross-validation to find optimal hyperparameters of XGBoost
+import numpy as np
+import xgboost as xgb
+import pickle
+from sklearn.model_selection import GridSearchCV
+from data_generation import get_data
+
+
+def xgb_cv(y_data, d_data, x_data, cv=5):
+    xgb_model_g = xgb.XGBRegressor(objective='reg:squarederror', seed=0)
+    xgb_model_m = xgb.XGBClassifier(objective='binary:logistic', seed=0)
+
+    param_grid = {
+        'n_estimators': [50, 75, 100, 150, 200],
+        'max_depth': [2],
+        'subsample': [0.6, 0.8],
+        'learning_rate': [0.03, 0.06, 0.1],
+        'reg_lambda': [0.01, 0.1, 1, 10, 100]
+    }
+
+    grid_search_g = GridSearchCV(estimator=xgb_model_g, param_grid=param_grid, cv=cv, n_jobs=-1,
+                                 scoring='neg_mean_squared_error')
+    grid_search_m = GridSearchCV(estimator=xgb_model_m, param_grid=param_grid, cv=cv, n_jobs=-1,
+                                 scoring='neg_brier_score')
+
+    xgb_params_dict = {}
+    for d in [0, 1]:
+        grid_search_g.fit(X=x_data[d_data == d], y=y_data[d_data == d])
+        xgb_params_dict[f'g{d}'] = grid_search_g.best_params_
+    grid_search_m.fit(X=x_data, y=d_data)
+    xgb_params_dict['m'] = grid_search_m.best_params_
+
+    return xgb_params_dict
+
+
+# Perform cross-validation for all datasets
+sample_sizes = [250, 500, 1000, 2000, 4000, 8000, 16000]
+n_MC = 5000
+opt_params_xgb = {}
+
+for N in sample_sizes:
+    rng = np.random.default_rng(seed=123)
+    opt_params_xgb_N = {}
+    
+    for j in range(n_MC): 
+        y_data, d_data, x_data = get_data(N, rng)
+        opt_params_xgb_N[j] = xgb_cv(y_data, d_data, x_data)
+
+    opt_params_xgb[N] = opt_params_xgb_N
+    print(f'Cross-validation done for N={N}')
+
+with open('opt_params_xgb.pkl', 'wb') as pickle_file:
+    pickle.dump(opt_params_xgb, pickle_file)
--- Simple model (3 confounders) - Clip x_normal/dml_algorithm.py
+++ Simple model (3 confounders) - Clip x_normal/dml_algorithm.py
@@ -1,4 +1,4 @@
-# Parallelized DML algorithm
+# DML algorithm
 import numpy as np
 from joblib import Parallel, delayed
 from sklearn.model_selection import StratifiedKFold
@@ -13,12 +13,114 @@ def mm_ate(y_data, d_data, x_data):
     return np.mean(g_0(1, x_data) - g_0(0, x_data) + d_data*(y_data-g_0(1, x_data))/m_0(x_data) - (1-d_data)*(y_data-g_0(0, x_data))/(1-m_0(x_data)))
 
 
-# Infeasible method-of-moments (oracle) estimator of the ATT
-def mm_att(y_data, d_data, x_data):
-    return np.mean(d_data*(y_data-g_0(0, x_data)) - m_0(x_data)*(1-d_data)*(y_data-g_0(0, x_data))/(1-m_0(x_data))) / np.mean(d_data)
+# 'DML' estimator of the ATE without cross-fitting
+def dml_no_cf_ate(y_data, d_data, x_data, model_g, model_m, alpha=0.05, m_bounds=None):
+    # Estimate outcome regression functions g_0(d)
+    g_0_hat = []
+    for d in [0, 1]:
+        model_g[d].fit(X=x_data[d_data==d], y=y_data[d_data==d])
+        g_0_hat.append(model_g[d].predict(x_data))
+
+    # Estimate propensity score m_0
+    model_m.fit(X=x_data, y=d_data)
+    m_0_hat = model_m.predict_proba(x_data)[:,1]
+    if m_bounds is not None:
+        np.clip(m_0_hat, m_bounds[0], m_bounds[1], out=m_0_hat)
+    
+    # Compute ATE estimator
+    scores = g_0_hat[1] - g_0_hat[0] + d_data*(y_data-g_0_hat[1])/m_0_hat - (1-d_data)*(y_data-g_0_hat[0])/(1-m_0_hat)
+    estimate = np.mean(scores)
+
+    # Inference: estimate standard deviation and construct confidence interval
+    sigma_hat = np.sqrt(np.mean((scores-estimate)**2))
+    N = len(y_data)
+    quantile = norm.ppf(1-alpha/2)
+    CI = np.array([estimate-quantile*sigma_hat/np.sqrt(N), estimate+quantile*sigma_hat/np.sqrt(N)])
+
+    return estimate, sigma_hat, CI
 
 
-# DML estimator of the ATE
+# DML estimator of the ATE with cross-fitting, without parallelization
+def dml_ate(y_data, d_data, x_data_all, model_g, model_m, K=5, alpha=0.05, classical=True, errors=True, m_bounds=None):
+    # Check for transformed input features
+    if isinstance(x_data_all, list):
+        x_data_orig, x_data = x_data_all[0], x_data_all[1]
+    else:
+        x_data_orig, x_data = x_data_all, x_data_all
+
+    # Partition the data for cross-fitting
+    skf = StratifiedKFold(n_splits=K, shuffle=False)
+
+    # Compute respective ML estimators and thereupon auxiliary estimators
+    theta_0_check_list = []
+    scores_list = []
+    if classical:
+        reg_check_list, ipw_check_list = [], []
+    if errors:
+        rmse_list = []
+    
+    for (train_indices, eval_indices) in skf.split(X=x_data, y=d_data):
+        y_train, d_train, x_train = y_data[train_indices], d_data[train_indices], x_data[train_indices]
+        y_eval, d_eval, x_eval, x_eval_orig = y_data[eval_indices], d_data[eval_indices], x_data[eval_indices], x_data_orig[eval_indices]
+
+        # Estimate outcome regression functions g_0(d)
+        g_0_hat = []
+        for d in [0, 1]:
+            model_g[d].fit(X=x_train[d_train==d], y=y_train[d_train==d])
+            g_0_hat.append(model_g[d].predict(x_eval))
+
+        # Estimate propensity score m_0
+        model_m.fit(X=x_train, y=d_train)
+        m_0_hat = model_m.predict_proba(x_eval)[:,1]
+        if m_bounds is not None:
+            np.clip(m_0_hat, m_bounds[0], m_bounds[1], out=m_0_hat)
+            
+        # Compute auxiliary estimator
+        scores = g_0_hat[1] - g_0_hat[0] + d_eval*(y_eval-g_0_hat[1])/m_0_hat - (1-d_eval)*(y_eval-g_0_hat[0])/(1-m_0_hat)
+        theta_0_check_list.append(np.mean(scores))
+
+        # For variance estimation
+        scores_list.append(scores)
+
+        # For regression & IPW estimators
+        if classical:
+            reg_check_list.append(np.mean(g_0_hat[1] - g_0_hat[0])) 
+            ipw_check_list.append(np.mean(d_eval*y_eval/m_0_hat - (1-d_eval)*y_eval/(1-m_0_hat)))
+
+        # Assess RMSE of ML models on evaluation set
+        if errors:
+            rmse_g0 = root_mean_squared_error(g_0(0, x_eval_orig), g_0_hat[0])
+            rmse_g1 = root_mean_squared_error(g_0(1, x_eval_orig), g_0_hat[1])
+            rmse_m = root_mean_squared_error(m_0(x_eval_orig), m_0_hat)
+            rmse_list.append([rmse_g0, rmse_g1, rmse_m])
+
+    # Compute final estimator
+    theta_0_hat = np.mean(theta_0_check_list)
+    if classical:
+        reg_hat, ipw_hat = np.mean(reg_check_list), np.mean(ipw_check_list)
+
+    # Inference: estimate standard deviation and construct confidence interval
+    sigma_hat = np.sqrt(np.mean((np.array(scores_list)-theta_0_hat)**2))
+    N = len(y_data)
+    quantile = norm.ppf(1-alpha/2)
+    CI = np.array([theta_0_hat-quantile*sigma_hat/np.sqrt(N), theta_0_hat+quantile*sigma_hat/np.sqrt(N)])
+
+    # Average RMSEs across folds
+    if errors:
+        rmse = np.mean(rmse_list, axis=0)
+
+    # Return results
+    if classical and errors:
+        return np.array([theta_0_hat, reg_hat, ipw_hat]), sigma_hat, CI, rmse
+    elif classical and (not errors):
+        return np.array([theta_0_hat, reg_hat, ipw_hat]), sigma_hat, CI
+    elif (not classical) and errors:
+        return theta_0_hat, sigma_hat, CI, rmse
+    else:
+        return theta_0_hat, sigma_hat, CI
+
+
+# DML estimator of the ATE with cross-fitting, parallelized
 def dml_parallel_ate(y_data, d_data, x_data_all, model_g, model_m, K=5, alpha=0.05, classical=True, errors=True, m_bounds=None):
     # Check for transformed input features
     if isinstance(x_data_all, list):
@@ -102,78 +204,6 @@ def dml_parallel_ate(y_data, d_data, x_data_all, model_g, model_m, K=5, alpha=0.
     elif classical and (not errors):
         return np.array([theta_0_hat, reg_hat, ipw_hat]), sigma_hat, CI
     elif (not classical) and errors:
-        return theta_0_hat, sigma_hat, CI, rmse
-    else:
-        return theta_0_hat, sigma_hat, CI
-
-
-# DML estimator of the ATT
-def dml_parallel_att(y_data, d_data, x_data_all, model_g0, model_m, K=5, alpha=0.05, errors=True, m_bounds=None):
-    # Check for transformed input features
-    if isinstance(x_data_all, list):
-        x_data_orig, x_data = x_data_all[0], x_data_all[1]
-    else:
-        x_data_orig, x_data = x_data_all, x_data_all
-
-    # Process one data split in the cross-fitting procedure
-    def process_single_split(train_indices, eval_indices):
-        y_train, d_train, x_train = y_data[train_indices], d_data[train_indices], x_data[train_indices]
-        y_eval, d_eval, x_eval, x_eval_orig = y_data[eval_indices], d_data[eval_indices], x_data[eval_indices], x_data_orig[eval_indices]
-
-        # Estimate single nuisance function
-        def fit_predict(model, X, y):
-            model.fit(X, y)
-            if is_regressor(model):
-                return model.predict(x_eval)
-            else:
-                return model.predict_proba(x_eval)[:, 1]
-
-        # Estimate outcome regression function g_0(0) and propensity score m_0 in parallel
-        model_data_list = [(model_g0, x_train[d_train==0], y_train[d_train==0]), (model_m, x_train, d_train)]
-        g_0_hat, m_0_hat = Parallel(n_jobs=2)(delayed(fit_predict)(model, X, y) for model, X, y in model_data_list)
-        if m_bounds is not None:
-            np.clip(m_0_hat, m_bounds[0], m_bounds[1], out=m_0_hat)
-
-        # Compute auxiliary estimator and store scores for variance estimation
-        scores = d_eval*(y_eval-g_0_hat) - m_0_hat*(1-d_eval)*(y_eval-g_0_hat)/(1-m_0_hat)
-        theta_0_check = np.mean(scores)/np.mean(d_eval)
-
-        # Assess RMSE of ML models on evaluation set
-        if errors:
-            rmse_g0 = root_mean_squared_error(g_0(0, x_eval_orig), g_0_hat)
-            rmse_m = root_mean_squared_error(m_0(x_eval_orig), m_0_hat)
-        else:
-            rmse_g0, rmse_m = None, None
-
-        return theta_0_check, [scores, d_eval, np.mean(d_train)], [rmse_g0, rmse_m]
-
-    # Partition the data for cross-fitting
-    skf = StratifiedKFold(n_splits=K, shuffle=False)
-
-    # Cross-fitting, where the different splits are processed in parallel
-    results = Parallel(n_jobs=K)(delayed(process_single_split)(train_indices, eval_indices) for train_indices, eval_indices in skf.split(X=x_data, y=d_data))
-
-    # Collect results
-    theta_0_check_list = [result[0] for result in results]
-    scores_list = [result[1] for result in results]
-    if errors:
-        rmse_list = [result[2] for result in results]
-
-    # Compute final estimator
-    theta_0_hat = np.mean(theta_0_check_list)
-
-    # Inference: estimate standard deviation and construct confidence interval
-    sigma_hat = np.sqrt(np.mean(np.concatenate([(scores-theta_0_hat*d_eval)/d_bar_train for scores, d_eval, d_bar_train in scores_list])**2)) # list compre checken, alles concat
-    N = len(y_data)
-    quantile = norm.ppf(1-alpha/2)
-    CI = np.array([theta_0_hat-quantile*sigma_hat/np.sqrt(N), theta_0_hat+quantile*sigma_hat/np.sqrt(N)])
-
-    # Average RMSEs across folds
-    if errors:
-        rmse = np.mean(rmse_list, axis=0)
-
-    # Return results
-    if errors:
         return theta_0_hat, sigma_hat, CI, rmse
     else:
         return theta_0_hat, sigma_hat, CI
